bdf47811deca9a10ceccc0ec2aabac3d560db09f
Felipe Oliveira <felipexk1@live.com>
2020-10-01 04:27:36 -0300

    🚀 Add Recoil Js With Native support

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import {Navigation} from 'react-native-navigation';
+import React from 'react';
+import {RecoilRoot} from 'recoil';
 
 import Home from './src/screens/Home';
 
-Navigation.registerComponent('Home', () => Home);
+export function recoil(OriginalReactComponent) {
+  return function Component(props) {
+    return (
+      <RecoilRoot>
+        <OriginalReactComponent {...props} />
+      </RecoilRoot>
+    );
+  };
+}
+
+Navigation.registerComponent('Home', () => recoil(Home), Home);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
